a80d89f1d2c434500753c2018283723d5bfbf371
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-03-19 20:43:06 -0700

    comment form

--- src/comment_form.js
+++ src/comment_form.js
@@ -21,6 +21,22 @@ class CommentForm extends React.Component {
         type="text"
         placeholder="Your name..."
         style={ style.commentFormAuthor }
+        value={ this.state.author }
+        onChange={ this.handleAuthorChange }
+        />
+
+        <input
+        type="text"
+        placeholder="Say something..."
+        style={ style.commentFormText }
+        value={ this.state.text }
+        onChange={ this.handleTextChange }
+        />
+
+        <input
+        type="submit"
+        style={ style.commentFormPost }
+        value="Post"
         />
       </form>
     )
